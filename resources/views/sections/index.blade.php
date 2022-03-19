6b6a0dae181d780adf7c39006629e35a65718e02
Jamal Alawadi <eng.jamal1999@gmail.com>
2022-03-19 18:22:01 +0200

    System Dashboard

---
+++ resources/views/sections/index.blade.php
@@ -0,0 +1,133 @@
+@extends('layouts.master')
+@section('title', '   برنامج الفواتير | الاقسام')
+@section('css')
+
+<!-- Internal Data table css -->
+<link href="{{URL::asset('assets/plugins/datatable/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet" />
+<link href="{{URL::asset('assets/plugins/datatable/css/buttons.bootstrap4.min.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/plugins/datatable/css/responsive.bootstrap4.min.css')}}" rel="stylesheet" />
+<link href="{{URL::asset('assets/plugins/datatable/css/jquery.dataTables.min.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/plugins/datatable/css/responsive.dataTables.min.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/plugins/select2/css/select2.min.css')}}" rel="stylesheet">
+
+@endsection
+@section('page-header')
+				<!-- breadcrumb -->
+				<div class="breadcrumb-header justify-content-between">
+					<div class="my-auto">
+						<div class="d-flex">
+							<h4 class="content-title mb-0 my-auto">الاعدادات</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/ الاقسام</span>
+						</div>
+					</div>
+
+				</div>
+				<!-- breadcrumb -->
+@endsection
+@section('content')
+				<!-- row -->
+				<div class="row">
+
+
+                        <!--div-->
+                        <div class="col-xl-12">
+                            <div class="card mg-b-20">
+                                <div class="card-header pb-0">
+                                    <div class="d-flex justify-content-between">
+                                        <a class="modal-effect btn btn-outline-primary btn-block" data-effect="effect-scale" data-toggle="modal" href="#modaldemo8">اضافة قسم</a>
+                                        <i class="mdi mdi-dots-horizontal text-gray"></i>
+                                    </div>
+
+                                </div>
+                                <div class="card-body">
+                                    <div class="table-responsive">
+                                        <table id="example1" class="table key-buttons text-md-nowrap">
+                                            <thead>
+                                                <tr>
+                                                    <th class="border-bottom-0">#</th>
+                                                    <th class="border-bottom-0">اسم القسم</th>
+                                                    <th class="border-bottom-0">الملاحظات</th>
+                                                    <th class="border-bottom-0">العمليات </th>
+
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td>1</td>
+                                                    <td>System Architect</td>
+                                                    <td>Edinburgh</td>
+                                                    <td>61</td>
+
+                                                </tr>
+
+
+
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        	<!-- Basic modal -->
+		<div class="modal" id="modaldemo8">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content modal-content-demo">
+                    <form action="{{route('sections.store')}}" method="POST" >
+                        @csrf
+					<div class="modal-header">
+						<h6 class="modal-title">اضافة قسم</h6><button aria-label="Close" class="close" data-dismiss="modal" type="button"><span aria-hidden="true">&times;</span></button>
+					</div>
+					<div class="modal-body">
+						<div class="form-group">
+                          <label for="name">اسم القسم</label>
+                          <input type="text" name="name" id="name" class="form-control" placeholder="" aria-describedby="helpId">
+
+                        </div>
+                        <div class="form-group">
+                          <label for="description">الملاحظات</label>
+                          <textarea name="description" id="description" class="form-control" placeholder="" aria-describedby="helpId"></textarea>
+                        </div>
+
+					</div>
+					<div class="modal-footer">
+						<button class="btn ripple btn-success" type="submit">تأكيد</button>
+						<button class="btn ripple btn-secondary" data-dismiss="modal" type="button">إلغاء</button>
+					</div>
+                    </form>
+				</div>
+			</div>
+		</div>
+		<!-- End Basic modal -->
+
+				</div>
+				<!-- row closed -->
+			</div>
+			<!-- Container closed -->
+		</div>
+		<!-- main-content closed -->
+@endsection
+@section('js')
+
+<!-- Internal Data tables -->
+<script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.dataTables.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/responsive.dataTables.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/jquery.dataTables.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.bootstrap4.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.buttons.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.bootstrap4.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/jszip.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/pdfmake.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/vfs_fonts.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.html5.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.print.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/buttons.colVis.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{URL::asset('assets/plugins/datatable/js/responsive.bootstrap4.min.js')}}"></script>
+<!--Internal  Datatable js -->
+<script src="{{URL::asset('assets/js/table-data.js')}}"></script>
+<!-- Internal Modal js-->
+<script src="{{URL::asset('assets/js/modal.js')}}"></script>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\InvoicesController;
+use App\Http\Controllers\SectionController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Auth::routes();
+//هذا الكود يحتوي على تحديد المسارات التي سيتم تحميلها بواسطة المستخدم الذي يسجل الدخول بشكل صحيح
+// Auth::routes(['register' => false]);
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
+Route::resource('invoices', InvoicesController::class);
+Route::resource('sections', SectionController::class);
+
+
+
+Route::get('/{page}',[AdminController::class, 'index']);
